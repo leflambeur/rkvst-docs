80596598b210a696f720534e7fede557d281a3bc
William Godfrey <sernineleflambeur@gmail.com>
2021-05-23 14:57:27 +0100

    Copy to Clipboard test

--- assets/js/clipboard.js
+++ assets/js/clipboard.js
@@ -1,14 +1,16 @@
 import Clipboard from 'clipboard';
 
-var clipboard = new Clipboard('.btn-clipboard');
+var clipboard = new Clipboard('.btn-clipboard', {
+    target: function (trigger) {
+      return trigger.nextElementSibling;
+    },
+  });
 
 clipboard.on('success', function(e) {
-    /*
+    
     console.info('Action:', e.action);
     console.info('Text:', e.text);
     console.info('Trigger:', e.trigger);
-    */
-
     e.clearSelection();
 });
 
@@ -16,3 +18,15 @@ clipboard.on('error', function(e) {
     console.error('Action:', e.action);
     console.error('Trigger:', e.trigger);
 });
+
+
+var pre = document.getElementsByTagName('pre');
+for (var i = 0; i < pre.length; i++) {
+    var isLanguage = pre[i].children[0].className.indexOf('language-');
+    if ( isLanguage === 0 ) {
+        var button     = document.createElement("button")
+            button.className='btn-clipboard btn btn-link'
+            button.innerHTML='<span class="copy-status"></span>'
+            pre[i].prepend(button)
+        }
+}
